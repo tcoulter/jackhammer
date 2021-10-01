326f8d838fc5b239e31996009f8f1e4da9625213
Tim Coulter <tim@timothyjcoulter.com>
2021-10-01 01:12:05 -0700

    Create a persistent temp directory for the hardhat install

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const spawn = require('cross-spawn')
 const path = require("path");
 const fs = require("fs").promises;
 const findParentDir = require('find-parent-dir');
+const os = require("os");
 
 temp.track();
 
@@ -150,6 +151,9 @@ temp.mkdir('jackhammer', async function(err, dirPath) {
   let hardhatCLIPath = null;
   let spawnWorkingDirectory = currentWorkingDirectory;
 
+  // Try looking for hardhat in the current working directory 
+  // (or any parent folder with a node_modules folder).
+  // If found, we'll use that one. 
   try {
     let dir = findParentDir.sync(currentWorkingDirectory, "node_modules")
     let potentialPath = path.join(dir, "node_modules", "hardhat", "internal", "cli", "cli.js");
@@ -157,8 +161,25 @@ temp.mkdir('jackhammer', async function(err, dirPath) {
     hardhatCLIPath = potentialPath;
   } catch (e) {}
 
+  let persistentTempDir = path.join(os.tmpdir(), "__jackhammer_hardhat__");
+
+  // If it's not found in the current working directory, let's see if
+  // it's in the persistent temporary directory. We do this so that 
+  // we don't have to install it each time it's not found locally. 
   if (hardhatCLIPath == null) {
-    console.log("Hang tight, installing hardhat!");
+    try {
+      let potentialPath = path.join(persistentTempDir, "node_modules", "hardhat", "internal", "cli", "cli.js");
+      await fs.stat(potentialPath);
+      hardhatCLIPath = potentialPath;
+      spawnWorkingDirectory = persistentTempDir;
+    } catch (e) {}
+  }
+
+  // Still nothing? Alright, let's install it. 
+  if (hardhatCLIPath == null) {
+    await fs.mkdir(persistentTempDir, {recursive: true})
+
+    console.log("Hang tight, installing hardhat! (one-time, cached)");
     console.log("TIP: Jackhammer can skip this step if you run it within a project with hardhat installed.");
     console.log("")
 
@@ -172,7 +193,7 @@ temp.mkdir('jackhammer', async function(err, dirPath) {
         "hardhat"
       ], {
         stdio: "ignore",
-        cwd: dirPath
+        cwd: persistentTempDir
       })
 
       child.on('exit', function() {
@@ -185,8 +206,8 @@ temp.mkdir('jackhammer', async function(err, dirPath) {
     console.log("")
     console.log("")
 
-    hardhatCLIPath = path.join(dirPath, "node_modules", "hardhat", "internal", "cli", "cli.js");
-    spawnWorkingDirectory = dirPath;
+    hardhatCLIPath = path.join(persistentTempDir, "node_modules", "hardhat", "internal", "cli", "cli.js");
+    spawnWorkingDirectory = persistentTempDir;
   }
 
   let args = [
